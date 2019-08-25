e2242c0e3cdb85baa0b0320b75f45fb85a2bb096
Christoph Keller <christoph.keller@bambit.ch>
2019-08-25 11:51:59 +0200

    Bugfix Token Shizzle...

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,34 @@ import { mapState } from "vuex";
 
 export default {
   mounted() {
-    this.$store.dispatch(TOKEN_PROCESS);
     if (this.$store.getters.isAuthenticated) {
       window.newsfeed.startConnection(this.$store.getters.accessToken);
+
+      this.$store.dispatch(TOKEN_PROCESS)
+      .catch(() => {
+        this.$bvToast.toast("Das gewünschte Token wurde nicht gefunden oder wurde bereits eingelöst. Leider konnten wir Dir keine Punkte gutschreiben.", {
+          title: "Fehler beim Einlesen des Tokens",
+          variant: "warning",
+          solid: true,
+          autoHideDelay: 5000
+        });
+      });
     }
     this.$store.watch(
       (state, getters) => getters.isAuthenticated,
       (newValue, oldValue) => {
         if (newValue) {
           window.newsfeed.startConnection(this.$store.getters.accessToken);
+
+          this.$store.dispatch(TOKEN_PROCESS)
+            .catch(() => {
+              this.$bvToast.toast("Das gewünschte Token wurde nicht gefunden oder wurde bereits eingelöst. Leider konnten wir Dir keine Punkte gutschreiben.", {
+                title: "Fehler beim Einlesen des Tokens",
+                variant: "warning",
+                solid: true,
+                autoHideDelay: 5000
+              });
+            });
         } else {
           window.newsfeed.stopConnection();
         }
