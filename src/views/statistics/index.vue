fa45b8ef18a567fdc642b8c26c1710f7c905c98a
Christoph Keller <christoph.keller@bambit.ch>
2019-08-25 03:30:02 +0200

    Statistic page implemented.

---
+++ src/views/statistics/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <block-box class="statistics-page">
+    <h1>Punkte Statistik mit Vorhersage</h1>
+    <p>Nachfolgende Statistik zeigt den anhand der gesammelten Daten extrapolierten zeitlichen Verlauf.</p>
+    <div class="image-container">
+      <img class="stats-image" src="https://baernhaecktforecasting.azurewebsites.net/forecasting" />
+    </div>
+  </block-box>
+</template>
+
+<script>
+
+export default {
+  name: "Statistics",
+  mounted() {
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss">
+  .statistics-page {
+    .image-container {
+      padding: 30px;
+
+      .stats-image {
+        width: 100%;
+      }
+    }
+  }
+</style>
