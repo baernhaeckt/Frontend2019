6a4ad1b205588059fd25a4d26339fd6d21a8a266
Christoph Keller <christoph.keller@bambit.ch>
2019-12-26 20:51:46 +0100

    Urls exported in .env file, forecast image url configurabe in settings / .env file.

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,5 @@
 export const settings = {
-  APP_NAME: "Leaf",
-  API_SERVER: "https://baernhaeckt.azurewebsites.net/"
+  APP_NAME: "leaf",
+  API_SERVER: process.env.VUE_APP_BASE_URL,
+  FORECAST_URL: process.env.VUE_APP_FORECAST_URL
 };
--- src/views/statistics/index.vue
+++ src/views/statistics/index.vue
@@ -3,7 +3,7 @@
     <h1>Punkte Statistik mit Vorhersage</h1>
     <p>Nachfolgende Statistik zeigt den anhand der gesammelten Daten extrapolierten zeitlichen Verlauf.</p>
     <div class="image-container loading-container">
-      <img class="stats-image" src="https://baernhaecktforecasting.azurewebsites.net/forecasting" @load="isLoading = false" />
+      <img class="stats-image" :src="forecastImageUrl" @load="isLoading = false" />
       <div class="loader text-center" v-if="isLoading">
         <b-spinner class="large-spinner text-success" />
       </div>
@@ -12,55 +12,57 @@
 </template>
 
 <script>
+import { settings } from "@/settings";
 
 export default {
   name: "Statistics",
-  mounted() {
-  },
+  mounted() {},
   data() {
     return {
       isLoading: true
     };
   },
   computed: {
+    forecastImageUrl() {
+      console.log(settings);
+      return settings.FORECAST_URL;
+    }
   },
-  watch: {
-  },
-  methods: {
-  }
+  watch: {},
+  methods: {}
 };
 </script>
 
 <style lang="scss">
-  .statistics-page {
-    .image-container {
-      padding: 30px;
-      position: relative;
-      min-height: 200px;
+.statistics-page {
+  .image-container {
+    padding: 30px;
+    position: relative;
+    min-height: 200px;
 
-      .stats-image {
-        width: 100%;
-      }
+    .stats-image {
+      width: 100%;
+    }
 
-      .loader {
+    .loader {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+
+      background: #444;
+      opacity: 0.4;
+
+      .large-spinner {
+        width: 4rem;
+        height: 4rem;
+
         position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-
-        background: #444;
-        opacity: 0.4;
-
-        .large-spinner {
-          width: 4rem;
-          height: 4rem;
-
-          position: absolute;
-          top: calc(50% - 2rem);
-          left: calc(50% - 2rem);
-        }
+        top: calc(50% - 2rem);
+        left: calc(50% - 2rem);
       }
     }
   }
+}
 </style>
