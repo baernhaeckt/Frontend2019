cfe28b818359a97ed2ff89bd39d55c7df4b40893
Christoph Keller <christoph.keller@bambit.ch>
2020-01-01 12:24:50 +0100

    PointsList Update on Event added.

--- src/newsfeed.js
+++ src/newsfeed.js
@@ -1,5 +1,6 @@
 import { RANKINGS_LIST_GLOBAL, RANKINGS_LIST_FRIENDS, RANKINGS_LIST_LOCAL, RANKINGS_SUMMARY } from '@/store/actions/rankings'
 import { AWARDS_LIST } from '@/store/actions/awards'
+import { POINTS_LIST } from '@/store/actions/points'
 
 import { HubConnectionBuilder, LogLevel } from '@aspnet/signalr'
 import { settings } from '@/settings'
@@ -15,6 +16,7 @@ function refreshDataForEvent (event) {
       vueStore.dispatch(RANKINGS_LIST_FRIENDS)
       vueStore.dispatch(RANKINGS_LIST_LOCAL, vueStore.getters.getProfile.postalCode)
       vueStore.dispatch(RANKINGS_SUMMARY)
+      vueStore.dispatch(POINTS_LIST, vueStore.getters.getProfile.id)
       break
     case 'Neuer Award':
     case 'Award erhalten':
